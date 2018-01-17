ed189a1e31320b7046ee9a91bb47aa320a3eaacf
okaminouta <434563z@gmail.com>
2018-01-17 14:19:48 +0200

    refactor  2

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
     'use strict';
     angular.module('startApp', ['startApp.core',
         'startApp.services',
+        'startApp.request'
     ]).config(['cfpLoadingBarProvider', function (cfpLoadingBarProvider) {
         cfpLoadingBarProvider.includeSpinner = false;
     }]).run(['$state',function($state){
